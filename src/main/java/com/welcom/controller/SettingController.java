eb1ba8782fdee6beae3a04b555446fd89d93d1bb
herohe910623 <herohe910623@gmail.com>
2024-01-08 20:33:37 +0900

    test

---
+++ src/main/java/com/welcom/controller/SettingController.java
@@ -0,0 +1,11 @@
+package com.welcom.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class SettingController {
+
+
+}
